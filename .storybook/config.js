dfc125449b3aa97b9314839bfa852921e1c7696d
eneax <19818937+eneax@users.noreply.github.com>
2019-11-10 17:50:10 +0100

    Make stories accessible from src directory

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,13 @@
 import { configure } from '@storybook/react';
+// make CSS available to Storybook
+import '../src/index.css';
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src/stories', true, /\.stories\.js$/), module);
+// By default Storybook looks for stories in a /stories directory.
+// In this project, we want Storybook to look for stories inside /src directory
+const req = require.context('../src', true, /\.stories.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
