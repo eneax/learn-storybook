bba2942d465ab9735433f0ee9bcd0e184d3f14a8
eneax <19818937+eneax@users.noreply.github.com>
2019-11-12 21:23:18 +0100

    Add comments for Task stories

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -20,3 +20,15 @@ storiesOf('Task', module)
   .add('default', () => <Task task={task} {...actions} />)
   .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
   .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />)
+
+
+
+/*
+Storybook has 2 levels of organization: the component itself and its child stories.
+Each story is a variation of a component and you can have as many stories per component as you need.
+
+To initiate Storybook we first call the `storiesOf()` function to register the component.
+We add a display name for the component (i.e. 'default', 'pinned', 'archived')
+
+To define our stories, we call `.add()` once for each of our test states to generate a story.
+*/
