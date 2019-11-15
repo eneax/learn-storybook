e7a8c21261117a15266dbd9fba018d016d1cb62f
eneax <19818937+eneax@users.noreply.github.com>
2019-11-15 21:41:03 +0100

    Make stories folder as default story container

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,7 @@ import requireContext from 'require-context.macro'
 // make CSS available to Storybook
 import '../src/index.css'
 
-const req = requireContext('../src/components', true, /\.stories\.js$/);
+const req = requireContext('../src/stories', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
