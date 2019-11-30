5d1ac39f8de4e62512539f56f32f9e707a2a3bfc
eneax <19818937+eneax@users.noreply.github.com>
2019-11-30 20:25:27 +0100

    Create Content component to display the asset information

--- .storybook/addons/design-assets.js
+++ .storybook/addons/design-assets.js
@@ -1,6 +1,23 @@
-import React from "react"
+import React, { Fragment } from "react"
 import { AddonPanel } from "@storybook/components"
 import { addons, types } from "@storybook/addons"
+import { useParameter } from "@storybook/api"
+
+const Content = () => {
+  const results = useParameter("assets", []);
+
+  return (
+    <Fragment>
+      {results.length ? (
+        <ol>
+          {results.map(i => (
+            <li>{i}</li>
+          ))}
+        </ol>
+      ) : null}
+    </Fragment>
+  )
+}
 
 addons.register("my/design-assets", () => {
   addons.add("design-assets/panel", {
