9ce7919a699fd4b84aa9430dfc772660a762d9da
eneax <19818937+eneax@users.noreply.github.com>
2019-12-03 21:40:27 +0100

    Import design assest in storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,32 @@
-import { configure } from '@storybook/react'
+import { configure, addParameters } from '@storybook/react'
 import requireContext from 'require-context.macro'
 import 'storybook-chromatic'
 
 // make CSS available to Storybook
 import '../src/index.css'
 
-const req = requireContext('../src/stories', true, /\.stories\.js$/);
+addParameters({
+  assets: [
+    {
+      name: 'app',
+      url: '/designs/app.png',
+    },
+    {
+      name: 'items',
+      url: '/designs/items.png',
+    },
+    {
+      name: 'list 1',
+      url: '/designs/list-1.png',
+    },
+    {
+      name: 'list 2',
+      url: '/designs/list-2.png',
+    },
+  ],
+})
+
+const req = requireContext('../src/stories', true, /\.stories\.js$/)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
